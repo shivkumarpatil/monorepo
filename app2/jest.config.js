d61dcca3d142e44eb1c7c6be4b546b265f30e95a
shivkumarpatil <shivva45@gmail.com>
2024-10-21 01:21:09 +0530

    Create jest.config.js

---
+++ app2/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
+  ],
+  testPathIgnorePatterns: ['/node_modules/'],
+};
